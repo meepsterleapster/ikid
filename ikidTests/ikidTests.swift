f4eea672d7e145bad797fa081a34e15826717dab
George Lee <george10lee@gmail.com>
2025-04-26 22:59:17 -0700

    Initial Commit

---
+++ ikidTests/ikidTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikidTests.swift
+//  ikidTests
+//
+//  Created by George Lee on 4/26/25.
+//
+
+import Testing
+@testable import ikid
+
+struct ikidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
